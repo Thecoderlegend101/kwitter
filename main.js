c3480efff86cd09efa48c91cd2daf254e21d7a3f
Thecoderlegend101 <78315390+Thecoderlegend101@users.noreply.github.com>
2021-06-17 20:53:32 -0500

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-    function next() {
-        user_name = document.getElementById('input1').value
-        localStorage.setItem('user_name', user_name)
-        window.open("main_page.html")
-    }
